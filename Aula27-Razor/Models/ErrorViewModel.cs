f4535b6981d7fe56bf97db5a468a4c43a0ce2b01
Jaqueline <jaquerossetto@hotmail.com>
2022-07-12 16:31:28 -0300

    Adicionar arquivos de projeto.

---
+++ Aula27-Razor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aula27_Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
